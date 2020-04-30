d36f7d470f9f30c8f0dbb7ed55e9719879e944f2
Joshua Singleton <jsingle345@gmail.com>
2020-04-30 15:20:08 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   array = [1,2,3]
   count = 0
   while count < array.length do
-  array[count] = array[count]**
+  array[count] = array[count].exp
   count += 1 
 end
 array 
