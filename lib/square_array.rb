45a8ebed5ff4eb141df81a96e8164ad7bfd03e85
Joshua Singleton <jsingle345@gmail.com>
2020-04-30 15:17:48 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   array = [1,2,3]
   count = 0
   while count < array.length do
-  array[count] = array[count]**
+  array[count] = array[count].pow(2)
   count += 1 
 end
 array 
